bf913b4d2380d58e9aca8175a1131b2916ef1fa5
Hamza Altamas <hamza.altamas.01876@gmail.com>
2023-04-17 16:53:15 +0600

    about page added

--- HamzaMart-Ecommerce-Front-end/src/Pages/About.jsx
+++ HamzaMart-Ecommerce-Front-end/src/Pages/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import BreadCrumbBanner from '../components/Layout/BreadCrumbBanner'
 import Container from '../components/Layout/Container'
+import Flex from '../components/Layout/Flex'
+import { Link } from "react-router-dom";
+import Images from '../components/Layout/Images';
 
 const About = () => {
   return (
@@ -8,6 +11,38 @@ const About = () => {
     <Container>
 
      <BreadCrumbBanner/>
+     <Flex className="md:flex  gap-x-[20px]">
+        <div className="w-full md:w-[50%]">
+          <Link to="/about">
+           <Images imgsrc="https://i.postimg.cc/3rLvHbPz/Group-15.png"/>
+          </Link>
+        </div>
+         <div className="w-full md:w-[50%] mt-[20px] md:mt-0">
+       <Link to="/about">
+       <Images imgsrc="https://i.postimg.cc/3rLvHbPz/Group-15.png"/>
+      </Link>
+     </div>      
+      </Flex>
+      <h4 className='font-nunito text-[25px] md:text-[35px] mt-10 md:mt-[70px] lg:mt-[100px]'>
+          Hamza Mart is one of the world’s leading ecommerce brands and is internationally recognized for celebrating the essence of classic Worldwide cool looking style.
+          </h4>
+  
+      <Flex className=" md:flex gap-x-5 mt-10 md:mt-[70px] lg:mt-[100px]">
+            <div className="">
+              <h5 className='font-semibold mb-2 text-[16px]'>Our Vision</h5>
+              <p className='text-[12px] text-ashText'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            <div className="">
+              <h5 className='font-semibold mb-2 text-[16px]'>Our Story</h5>
+              <p className='text-[12px] text-ashText'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            <div className="">
+              <h5 className='font-semibold mb-2 text-[16px]'>Our Brands</h5>
+              <p className='text-[12px] text-ashText'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+      </Flex>
+         
+    
     </Container>
     </>
   )
--- HamzaMart-Ecommerce-Front-end/src/components/Layout/BreadCrumbBanner.jsx
+++ HamzaMart-Ecommerce-Front-end/src/components/Layout/BreadCrumbBanner.jsx
@@ -5,9 +5,10 @@ import {FaAngleRight} from 'react-icons/fa'
 const BreadCrumbBanner = ({title}) => {
   return (
       <div className='py-10 md:py-20 lg:py-24 '>
-          <h1 className='font-nunito font-bold text-[30px]  mb-3 md:text-[40px] lg:text-5xl'>{window.location.pathname.split("/")[1].charAt(0).toUpperCase() + window.location.pathname.split("/")[1].slice(1)}</h1>
+          <h1 className='font-nunito font-bold text-[25px]  mb-3 md:text-[35px] lg:text-5xl'>{window.location.pathname.split("/")[1].charAt(0).toUpperCase() + window.location.pathname.split("/")[1].slice(1)}</h1>
           {/* bread crumb */}
           <p className='flex font-nunito text-ashText items-center gap-x-3 ml-[2px]  text-[12px] md:text-base'>Home <FaAngleRight/> {window.location.pathname.split("/")[1].charAt(0).toUpperCase() + window.location.pathname.split("/")[1].slice(1)}</p>
+          
       </div>
   )
 }
